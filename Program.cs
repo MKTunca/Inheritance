ed77c881134fa091aff13545c344f309377afcc1
MKTunca <91753517+MKTunca@users.noreply.github.com>
2022-01-09 17:43:23 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Triangle triangle = new Triangle();
+            triangle.Result();
+            Square square = new Square();
+            square.Result();
+            Circle circle = new Circle();
+            circle.Result();
+            Console.ReadLine();
+        }
+        class Environment
+        {
+            public virtual void Result()
+            {
+                Console.WriteLine("Şeklin kenarlarını topla");
+            }
+        }
+        class Triangle:Environment
+        {
+
+        }
+        class Square : Environment
+        {
+
+        }
+        class Circle:Environment
+        {
+            public override void Result()
+            {
+                Console.WriteLine("Yarıçapını 2 pi le çarp");
+            }
+        }
+    }
+}
